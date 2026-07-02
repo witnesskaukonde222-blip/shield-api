e67f350c454ea4dc6fc8c0011e5ec218124a99dc
Witness <witnesskaukonde222@gmail.com>
2026-07-03 00:24:31 +0200

    Remove duplicate RsaKeyProperties bean registration

--- src/main/java/com/enterprise/shield/config/RsaKeyProperties.java
+++ src/main/java/com/enterprise/shield/config/RsaKeyProperties.java
@@ -1,10 +1,7 @@
 package com.enterprise.shield.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
-@ConfigurationProperties(prefix = "rsa")
 public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
 }
